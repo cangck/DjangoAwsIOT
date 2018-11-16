3f22fcf87359fbb5c966e57e7e70a1150707d849
cuco <cangckun@gmail.com>
2018-11-16 13:43:56 +0800

    添加djangorestframwork

--- AwsIotWeb/urls.py
+++ AwsIotWeb/urls.py
@@ -13,10 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers
 
+# Serializers define the API representation.
+from awsiot import views
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('awsiot.urls'))
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- awsiot/views.py
+++ awsiot/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
 from django.http.response import HttpResponse
 
 
 # Create your views here.
+from rest_framework import viewsets
+
+from awsiot.serializer.serializer import UserSerializer, GroupSerializer
+
 
 def index(request):
     return HttpResponse("AwsIotWeb");
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
